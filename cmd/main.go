35bd01a6b61c12886df83a8490b2389e9bac9b35
RX90 <aleksandrnikonov020@gmail.com>
2024-03-10 18:45:31 +0500

    Add godotenv, db

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	todo "github.com/RX90/Todo-App"
 	"github.com/RX90/Todo-App/pkg/handler"
 	"github.com/RX90/Todo-App/pkg/repository"
 	"github.com/RX90/Todo-App/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -15,11 +18,27 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository() 
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db) 
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
