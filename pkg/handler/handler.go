75deeb25c69ef7b44271f2495a5f55fbdb1489d4
RX90 <aleksandrnikonov020@gmail.com>
2024-03-10 16:55:19 +0500

    Add repository, service. Update handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,9 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/RX90/Todo-App/pkg/service"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
+	services *service.Service
+}
 
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
