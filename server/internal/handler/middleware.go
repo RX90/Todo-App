8b8b570c51e6801882522e278accaf6e664b9280
RX90 <aleksandrnikonov020@gmail.com>
2025-01-06 02:48:09 +0500

    Update input validation

--- server/internal/handler/middleware.go
+++ server/internal/handler/middleware.go
@@ -16,6 +16,7 @@ const (
 var ok = map[rune]bool{
 	'0': true, '1': true, '2': true, '3': true, '4': true,
 	'5': true, '6': true, '7': true, '8': true, '9': true,
+	' ': true,
 
 	'a': true, 'b': true, 'c': true, 'd': true, 'e': true, 'f': true, 'g': true,
 	'h': true, 'i': true, 'j': true, 'k': true, 'l': true, 'm': true, 'n': true,
