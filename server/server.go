2d07caa0a39478060969be859375d5322b5bc1be
RX90 <aleksandrnikonov020@gmail.com>
2025-04-02 00:00:06 +0500

    commented RunTLS func

--- server/server.go
+++ server/server.go
@@ -25,16 +25,16 @@ func (s *Server) Run(port string, handler http.Handler) error {
 	return s.httpServer.ListenAndServe()
 }
 
-func (s *Server) RunTLS(port, certFile, keyFile string, handler http.Handler) error {
-	s.httpServer = &http.Server{
-		Addr:           ":" + port,
-		Handler:        handler,
-		MaxHeaderBytes: 1 << 20, // 1MB
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-	}
-	return s.httpServer.ListenAndServeTLS(certFile, keyFile)
-}
+// func (s *Server) RunTLS(port, certFile, keyFile string, handler http.Handler) error {
+// 	s.httpServer = &http.Server{
+// 		Addr:           ":" + port,
+// 		Handler:        handler,
+// 		MaxHeaderBytes: 1 << 20, // 1MB
+// 		ReadTimeout:    10 * time.Second,
+// 		WriteTimeout:   10 * time.Second,
+// 	}
+// 	return s.httpServer.ListenAndServeTLS(certFile, keyFile)
+// }
 
 func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
